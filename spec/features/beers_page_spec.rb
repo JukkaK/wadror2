5c9e96135cceff6422eb3a4033d121022cf30f03
Jukka Koskelin <jukka.koskelin@helsinki.fi>
2014-02-07 14:26:14 +0200

    Tehtava 5

---
+++ spec/features/beers_page_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+include OwnTestHelper
+
+describe "Beers page" do
+  let(:user){ FactoryGirl.create(:user) }
+  let!(:brewery) { FactoryGirl.create :brewery, name:"Koff" }
+  let!(:beer1) { FactoryGirl.create :beer, name:"iso 3", brewery:brewery }
+
+  before :each do
+    sign_in(username:"Pekka", password:"Foobar1")
+  end
+
+  it "should not have any before been created" do
+    visit beers_path
+    expect(page).to have_content 'List'
+  end
+
+  it "should be able to add beer with valid name" do
+    visit new_beer_path
+    fill_in('beer[name]', with:'Yksikkotestikalja')
+    page.select('Lager', :from => 'beer[style]')
+    page.select('Koff', :from => 'beer[brewery_id]')
+
+    #puts page.html
+
+    expect{
+          click_button "Create Beer"
+        }.to change{Beer.count}.from(1).to(2)
+  end
+
+  it "should not be able to add beer with an invalid name" do
+    visit new_beer_path
+    fill_in('beer[name]', with:'')
+    page.select('Lager', :from => 'beer[style]')
+    page.select('Koff', :from => 'beer[brewery_id]')
+
+    #puts page.html
+
+    click_button "Create Beer"
+    expect(page).to have_content 'blank'
+    expect(Beer.count).to equal(1)
+
+
+  end
+
+
+end
--- spec/features/ratings_spec.rb
+++ spec/features/ratings_spec.rb
@@ -6,25 +6,32 @@ describe "Rating" do
   let!(:beer1) { FactoryGirl.create :beer, name:"iso 3", brewery:brewery }
   let!(:beer2) { FactoryGirl.create :beer, name:"Karhu", brewery:brewery }
   let!(:user) { FactoryGirl.create :user }
+  #let!(:rating) {FactoryGirl.create :rating, beer_id: 1, score:15}
+  #let!(:rating) {FactoryGirl.create :rating, beer_id: 1, score:20}
 
   before :each do
     sign_in(username:"Pekka", password:"Foobar1")
   end
 
+  #it "when adding ratings, number of ratings matches" do
+  #  visit ratings_path
+  #  except(page).to have_content("Number of ratings 2")
+  #end
+
 
   #Jostain syystä select... kaatuu ajonaikaisesti
-  #it "when given, is registered to the beer and user who is signed in" do
-  #  visit new_rating_path
-  #  select('iso 3', from:'rating[beer_id]')
-  #  fill_in('rating[score]', with:'15')
+  it "when given, is registered to the beer and user who is signed in" do
+    visit new_rating_path
+    page.select('iso 3', from:'rating[beer_id]')
+    fill_in('rating[score]', with:'15')
 
-  #  expect{
-  #    click_button "Create Rating"
-  #  }.to change{Rating.count}.from(0).to(1)
+    expect{
+      click_button "Create Rating"
+    }.to change{Rating.count}.from(0).to(1)
 
-  #  expect(user.ratings.count).to eq(1)
-  #  expect(beer1.ratings.count).to eq(1)
-  #  expect(beer1.average_rating).to eq(15.0)
-  #end
+   expect(user.ratings.count).to eq(1)
+    expect(beer1.ratings.count).to eq(1)
+    expect(beer1.average_rating).to eq(15.0)
+  end
 
 end
