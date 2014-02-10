8224feae89da4f192d9f96e810c8f40b2048d175
Jukka Koskelin <jukka.koskelin@helsinki.fi>
2014-02-10 15:50:10 +0200

    tehtava 1 jalkeiset toimet

---
+++ lib/beermapping_api.rb
@@ -0,0 +1,19 @@
+class BeermappingApi
+  def self.places_in(city)
+    url = "http://beermapping.com/webservice/loccity/#{key}/"
+
+    response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
+    places = response.parsed_response["bmp_locations"]["location"]
+
+    return [] if places.is_a?(Hash) and places['id'].nil?
+
+    places = [places] if places.is_a?(Hash)
+    places.inject([]) do | set, place |
+      set << Place.new(place)
+    end
+  end
+
+  def self.key
+    "a65b1c789c5d04cbfea12abf4fa75396"
+  end
+end
