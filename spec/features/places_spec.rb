a87bb8602ad4b3c28a9ae49c09bc5a12e0396c96
Jukka Koskelin <jukka.koskelin@helsinki.fi>
2014-02-14 10:29:18 +0200

    tehtava 2

--- spec/features/places_spec.rb
+++ spec/features/places_spec.rb
@@ -12,4 +12,29 @@ describe "Places" do
 
     expect(page).to have_content "Oljenkorsi"
   end
+
+  it "if more than one one is returned by the API, they are shown at the page" do
+    BeermappingApi.stub(:places_in).with("kumpula").and_return(
+        [ Place.new(:name => "Oljenkorsi"), Place.new(:name => "Juottola") ]
+    )
+
+    visit places_path
+    fill_in('city', with: 'kumpula')
+    click_button "Search"
+
+    expect(page).to have_content "Oljenkorsi"
+    expect(page).to have_content "Juottola"
+  end
+  it "if no places are returned by the API, it is shown at the page" do
+    BeermappingApi.stub(:places_in).with("kumpula").and_return(
+        [ ]
+    )
+
+    visit places_path
+    fill_in('city', with: 'kumpula')
+    click_button "Search"
+
+    expect(page).to have_content "No locations in"
+  end
+
 end
